3b59199801674a5423a8f0eb8db272df7f192b03
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-02 14:36:40 -0300

    repeticao "do" em java

---
+++ Numeros/src/numeros/Numeros.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numeros;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ingri
+ */
+public class Numeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n, s = 0;
+        String resp;
+        Scanner teclado = new Scanner(System.in);
+        do{
+            System.out.print("Digite um número: ");
+            n = teclado.nextInt();
+            s +=n; // s = s + n;
+            System.out.print("Quer continuar? [S/N]");
+            resp = teclado.next();
+        } while (resp.equals("S") || resp.equals("s"));
+        System.out.println("A soma de todos os valores é " + s);
+        
+    }
+    
+}

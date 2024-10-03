402c845ad19c7f4bab3c015a497cbc235fd606ba
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-03 18:20:51 -0300

    estudo estrutura repetição "for" aninhado em java

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author ingri
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int cc = 1;  cc <= 2; cc++){ 
+            System.out.println("cc " + cc);
+            for(int j = 1; j <=2; j++){
+                System.out.println("j " + j);
+            }
+        }
+        /*
+        int cc=0;
+        while (cc <= 4) {
+            System.out.println(cc);
+            cc++;
+        }              
+        */
+    }
+    
+}

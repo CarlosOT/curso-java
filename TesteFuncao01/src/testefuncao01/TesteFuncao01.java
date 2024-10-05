56e45e786c7d325719393ee18b0bddcdf297a2b8
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-05 16:24:58 -0300

    funcão e procedimento em java

---
+++ TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testefuncao01;
+
+/**
+ *
+ * @author ingri
+ */
+public class TesteFuncao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static void soma(int a, int b){
+        int s = a + b;
+        System.out.println("A soma de " + a + " + " + b + " é " + s);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Começou o programa");
+        soma(3,4);
+    }
+    
+}
